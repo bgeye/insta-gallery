0d8dce67807de4108867955ef527f6406ed35d0d
Mario Knecht <mario.knecht@gmail.com>
2018-03-14 08:25:22 +0100

    npm init, gulp runs

---
+++ app/gulpfile.js
@@ -0,0 +1,9 @@
+
+
+const gulp = require('gulp');
+
+
+gulp.task('default',function(){                     //run with 'gulp' or 'gulp default'
+   console.log('hey, hou, I m the default task');
+});
+
