372eebc9cd21ac7e3952a752bf482330bcb0622f
Mario Knecht <mario.knecht@gmail.com>
2018-03-15 08:36:42 +0100

    added browserSync and watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 
 
 const gulp = require('gulp');
+
+const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
 
 
@@ -9,6 +11,28 @@ gulp.task('default',function() {                     //run with 'gulp' or 'gulp 
 });
 
 gulp.task('sass',function() {
-    console.log('ich bin ein präprocessor, hoi du!');
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())                       //sass präprozessor
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+
+            stream: true                    //browserSync
+        }));
+
+});
+
+gulp.task('browserSync',function() {
+   browserSync.init({
+       server: {
+           baseDir: 'app'
+       },
+   }) ;
+});
+
+gulp.task('watch',['browserSync','sass'],function() {       //executed tasks after call of watch(sass for updates in scss before running task)
+
+    gulp.watch('app/scss/**/*.scss',['sass']);              //executed task after change in scss file
+    gulp.watch('app/**/*.html',browserSync.reload);
+    gulp.watch('app/js/**/*.js',browserSync.reload);
 
 });
